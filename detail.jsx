9da00de93d9d5e5f11bb4fd84ef3d9d84974bfc3
angelagracia <93326534+angelagracia@users.noreply.github.com>
2023-10-23 01:49:03 +0700

    Add files via upload

---
+++ detail.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Buku from '../logo.png'
+const detail = () => {
+    return (
+        <div>
+            <img className="buku" src={"Buku"} alt="buku" />
+            <h1 className="judul">The Summer I  Turned Pretty</h1>
+            <ul>Author: Jenny Han</ul>
+            <ul>Format : 276 pages</ul>
+            <ul>Published : May 5, 2009 by Simon & Schuster Books for Young Readers </ul>
+            <ul>ISBN : 9781416968238 (ISBN10: 1416968237)</ul>
+            <ul>Language : English</ul>
+        
+            <h2 className="synopsis">Synopsis</h2>
+            <p>As far as teenager Isabel Conklin is concerned, summer is the only season that matters. For Isabel, nicknamed “Belly,” winter and spring are merely waiting periods. Not only is her birthday in August, but each June, she, her mother, Laurel, and her brother, Steven, travel to Cousins Beach to spend three months with the Fisher family, at their beach house on the Atlantic Coast.
+The summer that Belly turns sixteen, major changes impact everyone staying at the Fisher’s beach house. Belly has blossomed into a beautiful young woman, and both boys notice the change in her appearance.</p>
+        </div>
+    )
+}
+
+export default detail

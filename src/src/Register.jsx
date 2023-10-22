0bc37cba6b867666b59ac1df646fdacee68292a4
angelagracia <93326534+angelagracia@users.noreply.github.com>
2023-10-22 18:15:00 +0700

    Add files via upload

---
+++ src/src/Register.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        email.preventDevault();
+        console.log(email);
+    }
+
+    return (
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" />
+                <label htmlfor="password">Password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" />
+                <button type="sumbit">Log In</button>
+            </form>
+            <button onClick={() => props.onFormSwitch('login')}> Already have an account? Log In</button>
+        </div>
+    )
+}
